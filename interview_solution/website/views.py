14a0fef02dfc35546e476bd3fe63c09d1b48ab1b
yejin <yejin013@naver.com>
2020-09-26 13:14:30 +0900

    findIDPW

--- interview_solution/website/views.py
+++ interview_solution/website/views.py
@@ -20,3 +20,19 @@ def signin(request):
             return render(request, 'signin.html', {'error': 'username or password is incorrect'})
     else:
         return render(request,'signin.html')
+
+def findIDPW(request):
+    if request.method == "POST":
+        userID = request.POST.get('userID', '')
+        phone = request.POST.get('phone', '')
+
+        try:
+            user = User.objects.filter(userID=userID, phone=phone)
+            if user is not None:
+                return render(request, 'resultIDPW.html', {'userID':user.userID, 'password':user.password})
+            else:
+                return render(request, 'findIDPW.html', {'error': 'username or password is incorrect'})
+        except:
+            return render(request, 'findIDPW.html', {'error': 'username or password is incorrect'})
+    else:
+        return render(request, 'findIDPW.html')
