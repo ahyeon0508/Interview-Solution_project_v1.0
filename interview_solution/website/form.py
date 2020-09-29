d84d5d790b2ebc46065d10bae8a4feaf28034423
hyewon11 <hwj1104@naver.com>
2020-09-30 03:37:18 +0900

    기존 django 방식 수정중

---
+++ interview_solution/website/form.py
@@ -0,0 +1,29 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.db.transaction import commit
+
+from .models import User
+from django import forms
+
+class SignupForm(UserCreationForm):
+    userID = forms.CharField(max_length=10)
+    username = forms.CharField(max_length=10)
+    password1 = forms.CharField(max_length=20)
+    password2 = forms.CharField(max_length=20)
+    phone = forms.CharField(max_length=11)
+    class Meta:
+        model = User
+        fields = ['userID', 'username', 'password1', 'password2', 'phone']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 != password2:
+            raise forms.ValidationError('Error')
+        return password2
+
+    def save(self):
+        user = super(SignupForm, self).save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
--- interview_solution/website/views.py
+++ interview_solution/website/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import User
+
 
 # Create your views here.
+
+@csrf_exempt
+def signup(request):
+    if request.method == "POST":
+        userID = request.POST['userID']
+        username = request.POST.get('username','')
+        password = request.POST.get('password', '')
+        passwordChk = request.POST.get('passwordChk', '')
+        phone = request.POST.get('phone', '')
+        school = request.POST.get('school','')
+        grade = request.POST.get('grade', '')
+        sClass = request.POST.get('sClass', '')
+        year = request.POST.get('year', '')
+
+        if password != passwordChk:
+            return render(request, 'signup.html',{'error':'wrong'})
+
+        else:
+            User.objects.create_user(userID=userID, username=username, password = password, phone = phone,school= school, grade= grade, sClass= sClass, year= year)
+            return render(request,'signup.html')
+    else:
+        return render(request, 'signup.html',{'error':'wrong'})
+    
