610b59ffa79ed22b8ea73c349f2f1ee1ade40bc5
yejin <yejin013@naver.com>
2020-09-30 17:42:58 +0900

    teacher

--- interview_solution/website/urls.py
+++ interview_solution/website/urls.py
@@ -4,6 +4,7 @@ from . import views, apis
 app_name = 'website'
 
 urlpatterns = [
-    path('signin/', views.signin, name='signin')
+    path('student/signin/', views.studentSignin, name='signin'),
     # path('signin/', apis.SigninUserAPI.as_view(), name='signin'),
+    path('teacher/signin/', views.teacherSignin, name='signin')
 ]
--- interview_solution/website/views.py
+++ interview_solution/website/views.py
@@ -2,10 +2,10 @@ from django.contrib.auth.hashers import check_password
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
-from .models import User
+from .models import User, Teacher
 
 @csrf_exempt
-def signin(request):
+def studentSignin(request):
     if request.method == "POST":
         userID = request.POST.get('userID', '')
         password = request.POST.get('password', '')
@@ -13,6 +13,24 @@ def signin(request):
         try:
             user = User.objects.get(userID=userID)
             if check_password(password, user.password):
+                request.session['user'] = user.userID
+                return render(request, 'signin.html', {'error' : '성공'})
+            else:
+                return render(request,'signin.html',{'error':'username or password is incorrect'})
+        except:
+            return render(request, 'signin.html', {'error': 'username or password is incorrect'})
+    else:
+        return render(request,'signin.html')
+
+def teacherSignin(request):
+    if request.method == "POST":
+        userID = request.POST.get('userID', '')
+        password = request.POST.get('password', '')
+
+        try:
+            user = Teacher.objects.get(userID=userID)
+            if check_password(password, user.password):
+                request.session['user'] = user.userID
                 return render(request, 'signin.html', {'error' : '성공'})
             else:
                 return render(request,'signin.html',{'error':'username or password is incorrect'})
