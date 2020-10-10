072446cd3cc80925311c5e4c0d37eebecf891a1e
ahyeon <ahyeon0508@naver.com>
2020-10-10 22:47:28 +0900

    css

--- interview_solution/interview_solution/urls.py
+++ interview_solution/interview_solution/urls.py
@@ -15,11 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-<<<<<<< HEAD
 from django.conf.urls.static import static
 from django.conf import settings
-=======
->>>>>>> b980a489352d40603aef40585dcf40e1bcb18854
 
 urlpatterns = [
     path('admin/', admin.site.urls),
