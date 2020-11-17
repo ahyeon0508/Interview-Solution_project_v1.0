c198b066eb51d11841414f7524ac99bd5b3c4ba3
yejin <yejin013@naver.com>
2020-11-17 17:59:28 +0900

    media

--- interview_solution/website/views.py
+++ interview_solution/website/views.py
@@ -200,6 +200,8 @@ def myVideoAjax(request):
 @csrf_exempt
 def myVideoDetail(request, reportID):
     report = Report.objects.get(id=reportID)
+    print(report.video1.url)
+    print(report.video1.path)
     return render(request, 'report.html', {'report':report})
 
 @csrf_exempt
